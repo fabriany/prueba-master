f734191d43bf75db2be658699a4645daf874e592
fforregor <fforregor@ext.falabella.cl>
2024-05-29 16:25:43 -0500

    refactor

--- 2-Nomenclatura/app.js
+++ 2-Nomenclatura/app.js
@@ -8,7 +8,7 @@ function f(x, y, z) {
 
 //Reemplaza los nombres de las variables con nombres más descriptivos que reflejen mejor su función.
 
-function f(sumando_x, sumando_y, factor_z) {
+function senoDelProducto(sumando_x, sumando_y, factor_z) {
     let suma_xy = sumando_x + sumando_y;
     let producto_xyz = suma_xy * factor_z;
     let senoDelProducto = Math.sin(producto_xyz);
