bb8e7e7ff357a5498cd4c317b55e59a7608e9a62
fforregor <fforregor@ext.falabella.cl>
2024-05-31 15:28:11 -0500

    docs - se agrega documentacion

--- 1-consumo-apis/src/main.ts
+++ 1-consumo-apis/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Prueba Master')
+  .setDescription('Objetivo de la prueba: Evaluar tus habilidades en Backend')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
